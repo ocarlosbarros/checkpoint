8d26c264103387bbed5b716b4d4a56634b2cdb2b
Carlos Eduardo de Barros Souza <carlos_dbs@hotmail.com>
2019-06-12 16:51:19 -0300

    Adicionar Usuario Dashboard

--- src/pontoDigital/Controllers/DashboardController.cs
+++ src/pontoDigital/Controllers/DashboardController.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using pontoDigital.Enums;
 using pontoDigital.Models;
 using pontoDigital.Repository;
 
@@ -7,7 +11,10 @@ namespace pontoDigital.Controllers
 {
     public class DashboardController : Controller
     {
-        private ComentarioRepository comentarioRepository = new ComentarioRepository();
+        #region "Imports" do Repository
+            private UsuarioRepository usuarioRepository = new UsuarioRepository();
+            private ComentarioRepository comentarioRepository = new ComentarioRepository();
+        #endregion
 
         [HttpGet]
         public IActionResult Index()
@@ -18,6 +25,64 @@ namespace pontoDigital.Controllers
         [HttpGet]
         public IActionResult CadastrarUsuario()
         {
+            //SelectListItem representa o  componente SELECT que será renderizado no html
+            var permissoesList = new List<SelectListItem>();//Cria uma lista select
+            
+            //Adiciona os campos do SelectListItem
+            permissoesList.Add(new SelectListItem
+            {
+                Text = "Select",
+                Value = ""
+            });
+
+            //Populando o option com os valores do enum
+            foreach (EnumPermissao valor in Enum.GetValues(typeof(EnumPermissao)))
+            {
+                permissoesList.Add(new SelectListItem { Text = Enum.GetName(typeof(EnumPermissao), valor), Value = valor.ToString() });
+            }
+            
+            //Enviando a lista de permissoes para view
+            ViewBag.Permissoes = permissoesList;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarUsuario(IFormCollection frmCadastrarUsuario)
+        {
+            //SelectListItem representa o  componente SELECT que será renderizado no html
+            var permissoesList = new List<SelectListItem>();//Cria uma lista select
+            
+            //Adiciona os campos do SelectListItem
+            permissoesList.Add(new SelectListItem
+            {
+                Text = "Select",
+                Value = ""
+            });
+
+            //Populando o option com os valores do enum
+            foreach (EnumPermissao valor in Enum.GetValues(typeof(EnumPermissao)))
+            {
+                permissoesList.Add(new SelectListItem { Text = Enum.GetName(typeof(EnumPermissao), valor), Value = valor.ToString() });
+            }
+            
+            //Enviando a lista de permissoes para view
+            ViewBag.Permissoes = permissoesList;
+
+            Usuario usuario = new Usuario
+            (
+            nome: frmCadastrarUsuario["nome"],
+            genero: frmCadastrarUsuario["genero"],
+            dataNascimento: DateTime.Parse(frmCadastrarUsuario["dataNascimento"]),
+            endereco: frmCadastrarUsuario["endereco"],
+            email: frmCadastrarUsuario["email"],
+            telefone: frmCadastrarUsuario["telefone"],
+            senha:frmCadastrarUsuario["senha"]
+
+            );//Fim do construtor
+
+            usuarioRepository.Cadastrar(usuario);
+
+            ViewBag.titulo = "Cadastro";
             return View();
         }
 
