3d48fca1f078e6905ea373e9f1e7ca187dac0316
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-17 23:34:00 -0300

    feat(UserService):create a UserService

---
+++ src/Services/UserService.cs
@@ -0,0 +1,18 @@
+using CheckPoint.Interfaces;
+
+namespace CheckPoint.Services;
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository) => _userRepository = userRepository;
+
+    public void CreateUser(User user) {}
+
+    public User GetUserBy(string email, string password)
+    {
+        var user = _userRepository.GetUserBy(email, password);
+
+        return user;
+    }
+}
