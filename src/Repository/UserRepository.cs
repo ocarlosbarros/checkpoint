99b7459d6b4aa07d1309269152697db3ed2e1f6b
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-19 18:51:58 -0300

    feat(UserRepository):implements createUser method

--- src/Repository/UserRepository.cs
+++ src/Repository/UserRepository.cs
@@ -9,8 +9,12 @@ public class UserRepository : IUserRepository
     private readonly CheckPointContext _dbContext;
 
     public UserRepository(CheckPointContext dbContext) => _dbContext = dbContext;
-    
-    public void CreateUser(User user){}
+
+    public void CreateUser(User user)
+    {
+        _dbContext.Users.Add(user);
+        _dbContext.SaveChanges();
+    }
 
     public User GetUserBy(string email, string password)
     {
