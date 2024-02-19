cb27b59ba1af8ca270848837c53ca92feb40ffde
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-19 18:50:55 -0300

    refactor(User):Change the name class, properties and methods to english

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,5 +1,6 @@
 using System;
 using CheckPoint.Enums;
+using CheckPoint.Models;
 
 public class User {
 
@@ -11,5 +12,44 @@ public class User {
         public string Telephone { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public EnumPermissao Permission {get;set;}
+        
+        public EnumPermission Permission {get;set;}
+        
+        public User(){}
+        
+        public User(string name, string gender, DateTime dateOfBirth, string address, string telephone, string email, string password)
+        {
+                this.Name = name;
+                this.Gender = gender;
+                this.DateOfBirth = dateOfBirth;
+                this.Address = address;
+                this.Telephone = telephone;
+                this.Email = email;
+                this.Password = password;
+        }
+        
+        public User(int id, string name, string gender, DateTime dateOfBirth, string address, string telephone, string permission, string email, string password)
+        {
+                this.Id = id;
+                this.Name = name;
+                this.Gender = gender;
+                this.DateOfBirth = dateOfBirth;
+                this.Address = address;
+                this.Telephone = telephone;
+                this.Permission = (EnumPermission) Enum.Parse(typeof(EnumPermission), permission, true);
+                this.Email = email;
+                this.Password = password;
+        }
+        
+        public void ApproveOrDisapproveComment(Comment comment)
+        {
+                if(comment.Status)
+                {
+                        comment.Status = false;
+                
+                }else
+                {
+                        comment.Status = true;
+                }
+        }
 }
